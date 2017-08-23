b71b96ad983541616437e621034d306982d6a10e
rsrahul1000 <rsrahul1000@gmail.com>
2017-08-23 17:16:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('article-one', function (req, res) {
-    res.send('article one is going to be called and served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one'));
 });
 
 app.get('article-two', function (req, res) {
-    res.send('article two is going to be called and served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two'));
 });
 
 app.get('article-three', function (req, res) {
-    res.send('article three is going to be called and served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-threel'));
 });
 
 app.get('/ui/style.css', function (req, res) {
